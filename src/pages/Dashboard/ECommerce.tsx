8f19bced14b48f07fa1910afc39740f6fa6d4f16
Nandgreat <paul4nank@gmail.com>
2023-05-30 15:56:45 +0100

    Dashboard items to show

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -8,35 +8,23 @@ import ChatCard from '../../components/ChatCard.tsx';
 import MapOne from '../../components/MapOne.tsx';
 import TableOne from '../../components/TableOne.tsx';
 import DefaultLayout from '../../layout/DefaultLayout.tsx';
-import { useAppSelector } from '../../app/hook.ts';
-import { dashboardItems } from '../../features/dashboard/dashboardActions.ts';
 import { KeyIcons, UsersIcons } from '../../icons/icons.tsx';
+import { useGetDetailsQuery } from '../../app/services/auth/authService.ts';
 
 const ECommerce = () => {
 
-  const dispatch = useDispatch()
-
-  const {loading, dashboardCount, error} = useAppSelector((state: any) => state.dashboardSlice)
-
-  // redirect authenticated user to profile screen
-   useEffect(() => {
- 
-    console.log(dashboardCount, "ffffffffffffffffffff");
-    
-   }, [dashboardCount])
- 
-
-  useEffect(() => {
-    dispatch(dashboardItems({}))
-
-  }, [])
+  const { data } = useGetDetailsQuery('dashboard/items', {
+    pollingInterval: 0, // 15mins
+    refetchOnMountOrArgChange: true,
+    refetchOnFocus: true
+  })
 
   return (
     <DefaultLayout>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne value={dashboardCount.workers_count} label={"No of Workers"} icon={UsersIcons} />
-        <CardOne value={dashboardCount.keys_count} label={"No of Keys"} icon={KeyIcons} />
-        <CardOne value={dashboardCount.workers_count} label={"No of Departments"} icon={UsersIcons} />
+        <CardOne value={data.data.workers_count} label={"No of Workers"} icon={UsersIcons} />
+        <CardOne value={data.data.keys_count} label={"No of Keys"} icon={KeyIcons} />
+        <CardOne value={data.data.workers_count} label={"No of Departments"} icon={UsersIcons} />
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
