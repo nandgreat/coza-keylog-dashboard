3c78419464da406a2f17a4e55f0e4b7d058355e6
Nandgreat <paul4nank@gmail.com>
2023-05-30 16:18:43 +0100

    Data is made nullable

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -22,9 +22,9 @@ const ECommerce = () => {
   return (
     <DefaultLayout>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne value={data.data.dashboard_count.workers_count} label={"No of Workers"} icon={UsersIcons} />
-        <CardOne value={data.data.dashboard_count.keys_count} label={"No of Keys"} icon={KeyIcons} />
-        <CardOne value={data.data.dashboard_count.workers_count} label={"No of Departments"} icon={UsersIcons} />
+        <CardOne value={data?.data?.dashboard_count.workers_count} label={"No of Workers"} icon={UsersIcons} />
+        <CardOne value={data?.data?.dashboard_count.keys_count} label={"No of Keys"} icon={KeyIcons} />
+        <CardOne value={data?.data?.dashboard_count.workers_count} label={"No of Departments"} icon={UsersIcons} />
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
